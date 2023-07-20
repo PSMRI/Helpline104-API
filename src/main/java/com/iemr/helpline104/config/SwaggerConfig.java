6591e82e7f10f41442dbbebec1ca0b6b1c25f5b7
Vidyadevi Salunkhe <84471226+Vidyaaa24@users.noreply.github.com>
2023-07-20 15:33:34 +0530

    Update SwaggerConfig.java

--- src/main/java/com/iemr/helpline104/config/SwaggerConfig.java
+++ src/main/java/com/iemr/helpline104/config/SwaggerConfig.java
@@ -45,7 +45,7 @@ public class SwaggerConfig {
     private ApiInfo metaData() {
         ApiInfo apiInfo = new ApiInfo(
                 "Helpline104 API",
-                "The AMRIT Helpline104 provides various services like medical advice, counseling, grievance resolution, and support during health emergencies to help individuals in need.line104 API",
+                "The AMRIT Helpline104 provides various services like medical advice, counseling, grievance resolution, and support during health emergencies to help individuals in need.",
                 "1.0",
                 "Terms of service", new Contact("AMRIT", "https://psmri.github.io/PSMRI/", "amrit@piramalswasthya.org"), "",
                 "");
