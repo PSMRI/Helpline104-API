5a7278eb302b4622ec471b9da9c149e5c50b68a7
indraniBan <indraniBan@gmail.com>
2024-06-14 13:28:58 +0530

    swagger Configuration

--- src/main/java/com/iemr/helpline104/config/SwaggerConfig.java
+++ src/main/java/com/iemr/helpline104/config/SwaggerConfig.java
@@ -1,22 +1,24 @@
-/*
-* AMRIT – Accessible Medical Records via Integrated Technology
-* Integrated EHR (Electronic Health Records) Solution
-*
-* Copyright (C) "Piramal Swasthya Management and Research Institute"
-*
-* This file is part of AMRIT.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see https://www.gnu.org/licenses/.
-*/
+package com.iemr.helpline104.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new 
+             Info().title("Helpline 104 API").version("version").description("A microservice for the creation and management of beneficiaries."))
+                .addSecurityItem(new SecurityRequirement().addList("my security"))
+                .components(new Components().addSecuritySchemes("my security",
+                        new SecurityScheme().name("my security").type(SecurityScheme.Type.HTTP).scheme("bearer")));
+    }
+
+}
