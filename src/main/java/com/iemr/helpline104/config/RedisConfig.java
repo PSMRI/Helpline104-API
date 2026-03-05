230e2234bad1851af8a6e5a289c4cf9b1f8209dd
vanitha1822 <vanitha@navadhiti.com>
2026-03-05 19:17:49 +0530

    fix: redis issue

--- src/main/java/com/iemr/helpline104/config/RedisConfig.java
+++ src/main/java/com/iemr/helpline104/config/RedisConfig.java
@@ -1,18 +1,41 @@
+/*
+* AMRIT – Accessible Medical Records via Integrated Technology 
+* Integrated EHR (Electronic Health Records) Solution 
+*
+* Copyright (C) "Piramal Swasthya Management and Research Institute" 
+*
+* This file is part of AMRIT.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
 package com.iemr.helpline104.config;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.iemr.helpline104.data.users.M_User;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
 	@Bean
@@ -21,18 +44,24 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, M_User> redisTemplate(RedisConnectionFactory factory) {
-		RedisTemplate<String, M_User> template = new RedisTemplate<>();
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(factory);
 
-		// Use StringRedisSerializer for keys (userId)
-		template.setKeySerializer(new StringRedisSerializer());
-
-		// Use Jackson2JsonRedisSerializer for values (Users objects)
 		Jackson2JsonRedisSerializer<M_User> serializer = new Jackson2JsonRedisSerializer<>(M_User.class);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		serializer.setObjectMapper(mapper);
 		template.setValueSerializer(serializer);
 
 		return template;
 	}
+
+	// new bean for rate limiting & counters
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        return new StringRedisTemplate(factory);
+    }
 
 }
